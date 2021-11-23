f0055345e294a9c9edd19fea45fca7d33cb76e2c
floriantum <florian.oberneder@tum.de>
2021-11-23 20:29:49 +0100

    Create get_sol.m
    New function to run the methods

---
+++ get_sol.m
@@ -0,0 +1,26 @@
+function [sol,time] = get_sol(usecase,method,opt)
+%
+% GET_SOL   Is used to solve a defined problem for the choosen usecase
+%   
+% Usage: sol,time = get_sol(usecase,method,opt)
+% 
+% Description
+% 
+% Inputs:
+%
+% Outputs:
+%
+% Authors: 
+%   Florian Oberneder
+%
+% References:
+%
+% Requires:
+%
+% TODO:
+%   1. Write body of the function
+%   2. Specify the description
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
