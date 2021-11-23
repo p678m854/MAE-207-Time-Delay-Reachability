cff75c489b97f1befa7a9f849785fc1e437917a0
floriantum <florian.oberneder@tum.de>
2021-11-23 21:07:20 +0100

    get_sol: body of function
    body of function was created

--- get_sol.m
+++ get_sol.m
@@ -18,9 +18,35 @@ function [sol,time] = get_sol(usecase,method,opt)
 % Requires:
 %
 % TODO:
-%   1. Write body of the function
-%   2. Specify the description
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%   1. Write body of the function -- FO
+%   2. Specify the description -- FO
+
+%% Usecase selection
+switch usecase
+   case 'pendulum_example'
+      load('pendulum_example.mat');
+   case 'satellite_example'
+      load('satellite_example.mat');
+   case 'quadcopter_example'
+      load('quadcopter_example.mat');
+   otherwise
+      disp('Please select a usecase')
+end
+
+fprintf('usecase %s is selected',usecase);
+
+%% Method selection
+switch method
+   case 'trinh_method1'
+      disp('method TRINH_METHOD1 is selected');
+      statements
+   case 'fridman_method1'
+      disp('method FRIDMAN_METHOD1 is selected');
+      statements
+   otherwise
+      disp('Please select a method')
+end
+
+fprintf('method %s is selected',usecase);
 end
 
