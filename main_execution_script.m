43e058525d13f5792e4b7da68ee6d755672f23c9
floriantum <florian.oberneder@tum.de>
2021-11-24 04:23:48 +0100

    Update main_execution_script.m
    Updated examples

--- main_execution_script.m
+++ main_execution_script.m
@@ -8,12 +8,18 @@
 % Options:
 %     - 2D usecase: pendulum_example
 %     - 4D usecase: satellite_example
-%     - 10D usecase: quadcopter_example
+%     - 9D usecase: simple_quadcopter_example
+%     - 12D usecase: quadcopter_example
+% Additional Options for code verification:
+%     - 2D usecase: fridman_example
 % Uncomment to select the usecase:
 
-usecase = 'pendulum_example';
+% usecase = 'pendulum_example';
 % usecase = 'satellite_example';
+% usecase = 'simple_quadcopter_example';
 % usecase = 'quadcopter_example';
+
+% usecase = 'fridman_example';
 
 %% Method selection
 % Select a Mehtod for execution
@@ -22,13 +28,13 @@ usecase = 'pendulum_example';
 %     - fridman_method1
 % Uncomment to select the method:
 
- method = 'trinh_method1';
-% method = 'fridman_method1';
+% method = 'trinh_method1';
+method = 'fridman_method1';
 
 %% Execution
 %
 tic
-    [sol,time] = get_sol(usecase,method,opt);
-toc
+    [sol] = get_sol(usecase,method);
+time = toc;
 
-fprintf('The method %s has an execution time of %f seconds',method,timeit);
+fprintf('The Method %s has an execution time of %f seconds for Usecase %s \n',method,time,usecase);
