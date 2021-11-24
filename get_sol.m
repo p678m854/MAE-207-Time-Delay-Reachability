9bfe8a87b1da87053000987c5c6db83badd08aa4
floriantum <florian.oberneder@tum.de>
2021-11-24 04:23:04 +0100

    Update get_sol.m
    Update of the switch cases
    Input of the methods

--- get_sol.m
+++ get_sol.m
@@ -1,8 +1,8 @@
-function [sol,time] = get_sol(usecase,method,opt)
+function [sol] = get_sol(usecase,method)
 %
 % GET_SOL   Is used to solve a defined problem for the choosen usecase
 %   
-% Usage: sol,time = get_sol(usecase,method,opt)
+% Usage: sol,time = get_sol(usecase,method)
 % 
 % Description
 % 
@@ -24,29 +24,42 @@ function [sol,time] = get_sol(usecase,method,opt)
 %% Usecase selection
 switch usecase
    case 'pendulum_example'
-      load('pendulum_example.mat');
+      run('pendulum_example.m');
    case 'satellite_example'
-      load('satellite_example.mat');
+      run('satellite_example.m');
+   case 'simple_quadcopter_example'
+      run('simple_quadcopter_example.m');
    case 'quadcopter_example'
-      load('quadcopter_example.mat');
+      run('quadcopter_example.m');
+   case 'fridman_example'
+      run('fridman_example.m');
    otherwise
-      disp('Please select a usecase')
+      disp('Error! No usecase selected!')
 end
 
-fprintf('usecase %s is selected',usecase);
+fprintf('Usecase: %s is selected \n',usecase);
 
 %% Method selection
 switch method
    case 'trinh_method1'
-      disp('method TRINH_METHOD1 is selected');
-      statements
+      disp('Method: TRINH_METHOD1 is selected');
+      [beta0] = trinh_method1(A, Ad, B, ...
+                            E,...
+                            tau_m, tau_M,...
+                            d_m, d_M,...
+                            mu_scalar,...
+                            omega_scalar...
+                            );
+      sol = beta0;                                  
    case 'fridman_method1'
-      disp('method FRIDMAN_METHOD1 is selected');
-      statements
+      disp('Method: FRIDMAN_METHOD1 is selected');
+      [P, F, delta]= fridman_method1(A, Ad, B, ...
+                            tau_M, sqrt(omega_scalar)...
+                            );
+      sol = P;
    otherwise
-      disp('Please select a method')
+      disp('Error! No method selected!')
 end
 
-fprintf('method %s is selected',usecase);
 end
 
